15e886492d65e65b9b6dd1324f1a0b027862e10b
Francesca McConnell <fmcconnell59@gmail.com>
2022-03-04 18:15:34 -0700

    Add return first name method

--- src/Traveler.js
+++ src/Traveler.js
@@ -3,8 +3,20 @@ class Traveler {
     this.id = traveler.id;
     this.name = traveler.name;
     this.travelerType = traveler.travelerType;
+    this.trips = null;
+    this.currentTrip = null;
   }
 
+  returnFirstName() {
+    return this.name.split(' ')[0]
+  }
+
+  // returnCurrentTrip() {
+  //   this.trips.filter(trip => {
+  //     let durationDates = trip.date
+  //     if (trip.date === 
+  //   })
+  // }
 
 }
 
